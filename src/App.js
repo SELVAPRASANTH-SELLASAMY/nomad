d9703fe7747a19a97f772e41a1104162ada02841
selvaprasanth-sellasamy <prasanthsamy61@gmail.com>
2024-11-18 09:43:27 +0530

    Flash message added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { createContext, useRef } from 'react';
 import './App.css';
-import { Header, Login } from './components';
+import { Header, Login, Flash } from './components';
+const AppContext = createContext();
 function App() {
+  const flashMessage = useRef();
   return (
-    <>
+    <AppContext.Provider value={flashMessage}>
       <Header/>
       <Login/>
-    </>
+      <Flash/>
+    </AppContext.Provider>
   );
 }
 
 export default App;
+export { AppContext };
--- src/components/login/Setnewpassword.jsx
+++ src/components/login/Setnewpassword.jsx
@@ -1,9 +1,12 @@
 import './login.css';
 import { mainContext } from './Main';
+import { AppContext } from '../../App';
 import { useContext, useState } from 'react';
 import { useEvalPassword } from '../customhooks/validation';
+import { debounce } from 'lodash';
 function Setnewpassword(){
     const setFormState = useContext(mainContext);
+    const flashMessage = useContext(AppContext);
     const [userInput,setUserInput] = useState({
         newPassword:'',
         confirmPassword:''
@@ -14,6 +17,12 @@ function Setnewpassword(){
     });
     const passwordValidation = useEvalPassword(userInput.newPassword);
     const [check,setCheck] = useState(false); // For checkbox input to display password input.
+    const handleNewPasswordInput = debounce((e)=>{
+        setUserInput({...userInput,newPassword:e.target.value});
+    },1000);
+    const handleConfirmPasswordInput = debounce((e)=>{
+        setUserInput({...userInput,confirmPassword:e.target.value});
+    },1000);
     const validateInput = (input) => {
         const isEqual = input.newPassword === input.confirmPassword;
         setFormError({...formError,newPassword:input.newPassword === '' ? 'This field is required!' : passwordValidation ? '' : "Password doesn't meet the required criteria!",confirmPassword:input.confirmPassword === '' ? 'This field is required!' : isEqual ? '' :"Password's doesn't match!"});
@@ -23,8 +32,10 @@ function Setnewpassword(){
         if(validateInput(userInput)){
             //Logic to change password.
             console.log("Password changed successfully...");
+            flashMessage.current.alert("success","Password changed successfully.");
         }
     }
+    console.log("setNewPassword Re-Rendered...");
     return(
         <>
             <h2 className="text-primary font-weight-600 fs-8 italic">Reset password!</h2>
@@ -33,11 +44,11 @@ function Setnewpassword(){
             <form noValidate>
                 <label htmlFor="new-password" className="fs-4 d-iblock mtb-1">New password</label>
                 {formError.newPassword && <p className="text-error mtb-1 fs-4 float-right">{formError.newPassword}</p>}
-                <input onChange={(e)=>setUserInput({...userInput,newPassword:e.target.value})} type={check ? "text" : "password"} id='new-password' name='new-password' placeholder='Enter your new password' autoComplete='off'/>
+                <input onChange={(e)=>handleNewPasswordInput(e)} type={check ? "text" : "password"} id='new-password' name='new-password' placeholder='Enter your new password' autoComplete='off'/>
 
                 <label htmlFor="confirm-password" className="fs-4 d-iblock mtb-1">Confirm password</label>
                 {formError.confirmPassword && <p className="text-error mtb-1 fs-4 float-right">{formError.confirmPassword}</p>}
-                <input onChange={(e)=>setUserInput({...userInput,confirmPassword:e.target.value})} type={check ? "text" : "password"} id='confirm-password' name='confirm-password' placeholder='Confirm your new password' autoComplete='off'/>
+                <input onChange={(e)=>handleConfirmPasswordInput(e)} type={check ? "text" : "password"} id='confirm-password' name='confirm-password' placeholder='Confirm your new password' autoComplete='off'/>
 
                 <div className="d-iflex center-y">
                     <input onChange={(e)=>setCheck(e.target.checked)} type="checkbox" id='show-password' name='show-password'/>
