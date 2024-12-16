82bc586ae3188ff625e65501e99a56d3bf92599b
selvaprasanth-sellasamy <prasanthsamy61@gmail.com>
2024-12-16 12:26:45 +0530

    Added more actions buttons

--- src/components/blogtile/Blogtile.jsx
+++ src/components/blogtile/Blogtile.jsx
@@ -1,4 +1,32 @@
+import { useState } from 'react';
 import Lazyimage from '../lazyimage/Lazyimage';
+import { MdEdit, MdDelete } from "react-icons/md";
+import { PiDotsThreeOutlineVerticalFill } from "react-icons/pi";
+const Manageblog = () => {
+    const [showOptions,setShowOptions] = useState(false);
+    const options = [
+        {name:'Edit',icon:<MdEdit/>,clickHandler:(e) => {e.stopPropagation()}},
+        {name:'Delete',icon:<MdDelete/>,clickHandler:(e) => {e.stopPropagation()}}
+    ];
+    const handleMenuClick = (e) => {
+        e.stopPropagation();
+        setShowOptions(!showOptions);
+    }
+    return(
+        <div className='absolute top-8p right-8p d-flex flex-row-rev gap-05'>
+            <span role='button' tabIndex="0" onClick={handleMenuClick} title='More options' className={`pointer w-015rem aspect-ratio-equal bg-overlay rounded-100px d-flex center-y justify-center click-scale-down ${showOptions && 'rotate-90deg'}`}>
+                <PiDotsThreeOutlineVerticalFill/>
+            </span>
+            {
+                (showOptions && options) && options.map((option,index)=>(
+                    <span style={{animationDelay:`calc(${index} * 50ms)`}} role='button' tabIndex="0" key={index} onClick={option.clickHandler} title={option.name} className='pointer w-015rem aspect-ratio-equal bg-overlay rounded-100px d-flex center-y justify-center click-scale-down button-opa-effect'>
+                        {option.icon}
+                    </span>
+                ))
+            }
+        </div>
+    );
+}
 function Blogtile({blog}){
     const gotoBlog = () => {
         window.location.href = `blog?id=${blog._id}`;
@@ -11,7 +39,8 @@ function Blogtile({blog}){
         return `${day}-${month}-${year}`;
     }
     return(
-        <div onClick={gotoBlog} className='bg-tile-blue p-1 rounded-1 d-flex flex-col gap-05 pointer'>
+        <div onClick={gotoBlog} className='bg-tile-blue p-1 rounded-1 d-flex flex-col gap-05 relative'>
+            <Manageblog/>
             <Lazyimage 
                 componentClass={'w-100 aspect-ratio-21 rounded-top-1'} 
                 placeholder={blog.lazyImage} 
