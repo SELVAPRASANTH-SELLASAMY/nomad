ca2a169e511ffebaa812ea374f26853cf96d42f2
selvaprasanth-sellasamy <prasanthsamy61@gmail.com>
2024-12-31 20:29:27 +0530

    Added debouncing for useFetch

--- src/customhooks/httpMethod/useFetch.js
+++ src/customhooks/httpMethod/useFetch.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useMessage } from '../flash';
 import Axios from './utils/Axios';
 import { error } from './utils/error';
+import { debounce } from "lodash";
 const useFetch = (url) => {
     const [data,setData] = useState(null);
     const alert = useMessage();
@@ -9,15 +10,21 @@ const useFetch = (url) => {
         if(!url){
             return;
         }
-        Axios.get(url)
-        .then((res) => {
-            if(res.status === 200){
-                setData(res.data);
-            }
-        })
-        .catch((err) => {
-            error(err,alert);
-        })
+        const deboucing = debounce(() => {
+            Axios.get(url)
+            .then((res) => {
+                if(res.status === 200){
+                    setData(res.data);
+                }
+            })
+            .catch((err) => {
+                error(err,alert);
+            })
+        },[1000]);
+
+        deboucing();
+
+        return () => deboucing.cancel();
         //eslint-disable-next-line react-hooks/exhaustive-deps
     },[url]);
     return { data, setData };
