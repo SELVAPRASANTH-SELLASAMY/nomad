4975db7922c62d4801497e11ced67cfbed5bcce0
selvaprasanth-sellasamy <prasanthsamy61@gmail.com>
2025-01-21 09:57:57 +0530

    Rewritten style logic for search input

--- src/components/searchinput/Searchinput.jsx
+++ src/components/searchinput/Searchinput.jsx
@@ -1,29 +1,25 @@
 import { debounce } from "lodash";
-import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
 function Searchinput({setSearch}){
-    const [searchFocus,setSearchFocus] = useState(false);
-    
     const handleInput = debounce((e) => {
         const val = e.target.value;
         (val.length > 3 || val.length === 0) && setSearch(val);
     },[500]);
 
     return(
-        <div className={`fs-5 bg-common-blue d-iflex pl-1 center-y rounded-100px trans-border-250 ${searchFocus ? 'border-green-01' : 'border-grey-01'}`}>
+        <div className="search-field fs-5 bg-common-blue d-iflex pl-1 center-y rounded-100px trans-border-250">
             <IoSearchSharp/>
-            <input 
-                onFocus={()=>setSearchFocus(true)} 
-                onBlur={()=>setSearchFocus(false)} 
+            <input
                 onChange={handleInput}
-                className='fs-4 w-20rem ptb-05 rounded-100px no-border no-outline text-white bg-common-blue plr-05' 
+                className="search-input fs-4 w-20rem ptb-05 rounded-100px no-border no-outline text-white bg-common-blue plr-05"
                 type="search" 
-                id='search' 
-                name='search' 
-                placeholder='Search' 
-                autoComplete='off'
+                id="search" 
+                name="search" 
+                placeholder="Search" 
+                autoComplete="off"
             />
         </div>
     );
 }
+
 export default Searchinput;
