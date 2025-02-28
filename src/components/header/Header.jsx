cf0a222757c2ba7ccf6a73c4e7f637037b524ce6
selvaprasanth-sellasamy <prasanthsamy61@gmail.com>
2025-02-28 16:59:20 +0530

    Routing changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,10 +4,11 @@ import Searchinput from '../searchinput/Searchinput';
 import UserMenu from '../usermenu/UserMenu';
 function Header({setShowNavbar,setSearch}){
     const location = useLocation();
+    const sideBarChilds = ["/","/editor"];
     return(
         <>
             <header className={`w-100 ptb-025 plr-25 fixed left-0 top-0 d-flex center-y ${location.pathname !== '/login' && 'bg-tile-blue border-bottom-grey-01'}`}>
-                <h1 onClick={()=>setShowNavbar((prevState)=>!prevState)} className='text-primary fs-7 uppercase mr-2 pointer'>nomad</h1>
+                <h1 onClick={()=>setShowNavbar((prevState)=>!prevState)} className={`text-primary fs-7 uppercase mr-2 ${sideBarChilds.includes(location.pathname) && 'pointer'}`}>nomad</h1>
                 {location.pathname === '/' && 
                     <Searchinput setSearch={setSearch}/>
                 }
