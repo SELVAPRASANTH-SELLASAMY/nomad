f0da8e403907c6cf212a3f8998ce04a32afef683
selvaprasanth-sellasamy <prasanthsamy61@gmail.com>
2025-05-25 16:17:28 +0530

    Did some responsive optimisation

--- src/components/searchinput/Searchinput.jsx
+++ src/components/searchinput/Searchinput.jsx
@@ -7,7 +7,7 @@ function Searchinput({setSearch}){
     },[500]);
 
     return(
-        <div style={{marginLeft:"auto"}} className="search-field fs-5 fs-6_L_768 fs-5_5_L_500 bg-common-blue bg-none_L_768 d-iflex pl-1 pl-0_L_768 center-y rounded-100px trans-border-250 search-border-none_L_768">
+        <div style={{marginLeft:"auto"}} className="search-field fs-5 w-2_L_768 aspect-ration-equal_L_768 bg-common-blue d-iflex pl-1 p-05_L_768 center-y justify-center rounded-100px trans-border-250">
             <IoSearchSharp/>
             <input
                 onChange={handleInput}
