80008e0a87ad89cfbeb48d29fc9e5b9302a99f07
selvaprasanth-sellasamy <prasanthsamy61@gmail.com>
2025-06-15 12:41:58 +0530

    Changes in Option element mobile style

--- src/sharedUi/select/Option/Option.jsx
+++ src/sharedUi/select/Option/Option.jsx
@@ -1,7 +1,7 @@
 function Option({options,actions,setValue,value}){
     const opts = options || actions;
     return(
-        <ul id="OptionList" role="listbox" className="w-100 bg-tile-blue list-style-none fs-4 border-grey-01 hide-overflow rounded-05 mt-025 absolute static_L_768 left-0">
+        <ul id="OptionList" role="listbox" className="w-100 bg-tile-blue bg-tile-blue-tr_L_768 blur-10_L_768 list-style-none fs-4 border-grey-01 hide-overflow rounded-05 mt-025 absolute static_L_768 left-0">
             {opts &&
                 opts.map((option,index)=>(
                     <li onMouseDown={()=>setValue(option)} role="option" aria-selected={(option.name) ? (option.name === value.name) : (option.value) ? (option.value === value.value) : (value === option)} key={index} className="hover-blue plr-1 ptb-05 capitalize pointer d-flex gap-05">
