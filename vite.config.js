bdfa08f77a8f922ab1d083448e27211c702d099e
SELVAPRASANTH SELLASAMY <prasanthsamy61@gmail.com>
2026-06-21 11:43:36 +0530

    Second Commit

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig((command) => ({
   server:{
     port: 3000,
     open: true,
     host: true
   },
+  base: command === 'serve' ? '/nomad/' : '',
   plugins: [react()],
-})
+}))
